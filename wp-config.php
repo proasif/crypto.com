aac558aec9c7042587947518db9ba3e339cb033c
proasif <proasifparvez@gmail.com>
2018-09-01 13:15:24 +0530

    db sync

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp_crypto');
+define('DB_NAME', 'crypto_wp');
 
 /** MySQL database username */
 define('DB_USER', 'cryptoweb');
