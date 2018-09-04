d28d0f96642d4f7799feb732c643518adca29184
proasif <proasifparvez@gmail.com>
2018-09-04 18:29:32 +0530

    plugins added

---
+++ wp-content/uploads/sucuri/sucuri-auditqueue.php
@@ -0,0 +1,8 @@
+<?php
+// datastore=auditqueue;
+// created_on=1536065142;
+// updated_on=1536065142;
+exit(0);
+?>
+1536065142_172:"Warning: cryptoweb, 1.22.241.184; Plugin activated: Sucuri Security - Auditing, Malware Scanner and Hardening (v1.8.18; sucuri-scanner\/sucuri.php)"
+1536065142_4997:"Warning: cryptoweb, 1.22.241.184; Plugin activated: All-in-One WP Migration (v6.75; all-in-one-wp-migration\/all-in-one-wp-migration.php)"
