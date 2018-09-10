3f87a7401c82a2bede7fab382949c31dbef1b34b
proasif <proasifparvez@gmail.com>
2018-09-10 15:05:07 +0530

    commented google fb hotjar ID

--- wp-content/themes/pokelabs_crypto /functions.php
+++ wp-content/themes/pokelabs_crypto /functions.php
@@ -133,7 +133,7 @@ add_action( 'wp_head', 'headerRelatedFunctions', 10 );
 function insertFBSDK() {
 ?>
 	<!-- Start Facebook SDK Code -->
-	<script>
+	<!--<script>
 	window.fbAsyncInit = function() {
 		FB.init({
 			//appId      		  	 : '143487366003021', Commented appId
@@ -150,7 +150,7 @@ function insertFBSDK() {
      js.src = "https://connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
-	</script>
+	</script>-->
 	<!-- End Facebook SDK Code -->
 <?php	
 }
@@ -158,7 +158,7 @@ function insertFBSDK() {
 function insertFBPixel() {
 ?>
 	<!-- Facebook Pixel Code -->
-	<script>
+	<!--<script>
 	  !function(f,b,e,v,n,t,s)
 	  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 	  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -172,7 +172,7 @@ function insertFBPixel() {
 	</script>
 	<noscript><img height="1" width="1" style="display:none"
 	  src="https://www.facebook.com/tr?id=1717186665171468&ev=PageView&noscript=1"
-	/></noscript>
+	/></noscript>-->
 	<!-- End Facebook Pixel Code -->
 <?php	
 }
@@ -180,7 +180,7 @@ function insertFBPixel() {
 // 0.5  GOOGLE ANALYTICS
 function insertGA() {
 ?>
-	<script>
+	<!--<script>
 	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -189,7 +189,7 @@ function insertGA() {
 	  ga('create', 'UA-65289275-1', 'auto');
 	  ga('send', 'pageview');
 	
-	</script>
+	</script>-->
 <?php
 }
 
@@ -198,11 +198,11 @@ function insertGA() {
 function insertGTHeader() {
 ?>
 	<!-- Google Tag Manager -->
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	<!--<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-NF9LC5G');</script>
+	})(window,document,'script','dataLayer','GTM-NF9LC5G');</script>-->
 	<!-- End Google Tag Manager -->
 <?php
 }
@@ -233,13 +233,13 @@ add_action( 'init_body_start', 'insertGTBodyLogic');
 // 0.7 GOOGLE AUTO ADS
 function insertGoogleAutoAds() {
 ?>
-	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<!--<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 	<script>
 		 (adsbygoogle = window.adsbygoogle || []).push({
 			  google_ad_client: "ca-pub-3579343809189257",
 			  enable_page_level_ads: true
 		 });
-	</script>
+	</script>-->
 <?php
 }
 
@@ -247,7 +247,7 @@ function insertGoogleAutoAds() {
 function insertHotJarSDK() {
 	?>
 	<!-- Hotjar Tracking Code for https://aintviral.com -->
-	<script>
+	<!--<script>
 		(function(h,o,t,j,a,r){
 			h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
 		//	h._hjSettings={hjid:951085,hjsv:6};   Commented HotJar ID
@@ -256,7 +256,7 @@ function insertHotJarSDK() {
 			r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
 			a.appendChild(r);
 		})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-	</script>
+	</script>-->
 	<?php
 }
 
