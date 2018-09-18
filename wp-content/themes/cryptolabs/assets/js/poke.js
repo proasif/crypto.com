ccc7a3a315e9b3a5fc8208a43c2fc4d9410aeb59
Harsh Rajat <harsh.rajat@gmail.com>
2018-09-18 17:48:52 +0530

    Animated Footer

--- wp-content/themes/cryptolabs/assets/js/poke.js
+++ wp-content/themes/cryptolabs/assets/js/poke.js
@@ -1,5 +1,3 @@
-console.log("hello");
-
 /* Place all your JavaScript modifications below */
 $=jQuery.noConflict();
 
@@ -21,7 +19,8 @@ $(document).ready(function () {
 	
 	var datetime = new Date();
 	var p1TimePassed = datetime.getTime();	
-		 
+	var p1TimeBasedInitialCallDone = false;
+	
 	function p1TimeBasedCalls() {
 		// To animate Footer
 		animateFooter();
@@ -31,6 +30,7 @@ $(document).ready(function () {
 		p1TimePassed = datetime.getTime();
 		
 		// Recall the loop
+		p1TimeBasedInitialCallDone = true;
 		p1AnimBind( p1TimeBasedCalls );
 	}
 	
@@ -39,15 +39,52 @@ $(document).ready(function () {
 	
 	// 1.1 To Animate the Footer
 	function animateFooter() {
-		'use strict';
-		
 		var datetime = new Date();
 		var p1TimeDiff = parseFloat(datetime.getTime()) - parseFloat(p1TimePassed);
 		
-		console.log("Time: " + p1TimeDiff);
+		//movement is 50px of the width in 100ms
+		var movement = ((10/100) * p1TimeDiff);
+		var parallax0 = 0.4;
+		var parallax1 = 0.8;
+		var parallax2 = 1;
+		
+		// change the background accordingly
+		var $clouds = $("footer .clouds").first();
+		var cloudsX = parseFloat(getBgXPosition($clouds) - parseFloat(movement * parallax0));
+		
+		var $bg = $("footer .background").first();
+		var bgX = parseFloat(getBgXPosition($bg) - parseFloat(movement * parallax1));
+		
+		var $fg = $("footer .foreground").first();
+		var fgX = parseFloat(getBgXPosition($fg) - parseFloat(movement * parallax2));
+		
+		// reset to position 0 first initially 
+		if (!p1TimeBasedInitialCallDone) {
+			cloudsX = bgX = fgX = 0;
+		}
+		
+		$clouds.css({"background-position-x": cloudsX + "px"});
+		$bg.css({"background-position-x": bgX + "px"});
+		$fg.css({"background-position-x": fgX + "px"});
+		
+		console.log("Time: " + cloudsX);
+	}
+	
+	function getBgXPosition($object) {
+		
+		var backgroundPosition = $object.css('background-position');
+		
+		// backgroundPosition = "0% 0%" for example
+		var displacement = backgroundPosition.split(' '); // ["0%", "0%"]
+		
+		// As suggested, you could also get the float:
+		var xFloat = parseFloat(displacement[0].replace(/[^0-9-]/g, ''));
+		
+		return xFloat;
 	}
 	
 });
+
 
 function animateBackground() {
     $("footer .clouds").velocity({
