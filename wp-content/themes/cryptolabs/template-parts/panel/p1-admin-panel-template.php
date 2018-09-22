a5b565931444d99ce52ab9632a2ea04e9a8c8e88
proasif <proasifparvez@gmail.com>
2018-09-22 12:21:41 +0530

    Worked on template parts

--- wp-content/themes/cryptolabs/template-parts/panel/p1-admin-panel-template.php
+++ wp-content/themes/cryptolabs/template-parts/panel/p1-admin-panel-template.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The Template for displaying archive pages.
+ * Template for displaying entire admin panel
  *
  * @license For the full license information, please view the Licensing folder
  * that was distributed with this source code.
@@ -12,14 +12,8 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	die( 'No direct script access allowed' );
 }
-
-get_header();
 ?>
 
- 	<div id="primary" class="p1-primary-max">
-		<div id="content" role="main">
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
-
-<?php get_footer();
+          
+    
